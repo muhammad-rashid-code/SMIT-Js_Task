5d486c70308e833692ba439bc3ce3d71cffc8168
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2024-03-05 00:49:47 +0500

    Make attached screenshot using JS document.write() function.

---
+++ 02-JS-Hello-World/script.js
@@ -0,0 +1,6 @@
+let heading =
+  "<h1 class='cen'>Hello <span class='green'>JavaScript</span> Planet</h1>";
+let lineOne = "<p class='cen'>I am the <b>master</b> of my own fate.</p>";
+let lineTwo = "<p class='cen'>I am the <b>champion</b> of my own soule.</p>";
+let lineThree = "<p class='cen'>I am the <b>superhero</b> of my own life.</p>";
+document.write(heading, lineOne, lineTwo, lineThree);
