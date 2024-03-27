f6898f55abbc47401799d829ff3192e332d482b8
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2024-03-28 02:28:43 +0500

    resume for loop

--- JavaScript-Array/script.js
+++ JavaScript-Array/script.js
@@ -62,3 +62,79 @@
 // var uR = uName.slice(1, 30);
 
 // alert(uC + uR);
+
+// let fruits = [
+//   "0melon",
+//   "1apple",
+//   "2banana",
+//   "3gav",
+//   "4grapes",
+//   "5Water Melon",
+//   "6Mango",
+//   "7chicko",
+// ];
+
+// // console.log(fruits[5]);
+
+// // console.log(fruits.slice(5, 8));
+
+// fruits.splice(2, 2, "cat", "goat", "dog");
+
+// console.log(splice);
+
+// let primeNums = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23];
+
+// // Array.splice(startingIndex, delete item count, add new item(s))
+// primeNums.splice(2, 3, 29, 31, 37);
+
+// console.log(primeNums);
+
+// let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(num.length);
+
+// console.log(num.splice(2, 1, 11, 12, 13, 14));
+
+// num.splice(1, 2, 11, 23, 33, 44, 55);
+// console.log(num.splice(1, 2, 11, 23, 33, 44, 55));
+
+// let fruits = [
+//   "0melon",
+//   "1apple",
+//   "2banana",
+//   "3gav",
+//   "4grapes",
+//   "5Water Melon",
+//   "6Mango",
+//   "7chicko",
+// ];
+
+// fruits.splice(1, 2, "cat", "dog");
+// console.log(fruits);
+
+// let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// num.reverse;
+// console.log(num.reverse());
+// console.log(num.reverse().join(" + "));
+
+// let matrix1 = [
+//   [2, 3, 4],
+//   [1, 2, 3],
+//   [5, 3, 2],
+// ];
+// let matrix2 = [
+//   [1, 5, 9],
+//   [9, 5, 2],
+//   [3, 1, 5],
+// ];
+
+// console.log(matrix1[0][0] + matrix2[0][0]);
+// console.log(matrix1[0][1] + matrix2[0][1]);
+// console.log(matrix1[0][2] + matrix2[0][2]);
+
+// console.log(matrix1[1][0] + matrix2[1][0]);
+// console.log(matrix1[1][1] + matrix2[1][1]);
+// console.log(matrix1[1][2] + matrix2[1][2]);
+
+// console.log(matrix1[2][0] + matrix2[2][0]);
+// console.log(matrix1[2][1] + matrix2[2][1]);
+// console.log(matrix1[2][2] + matrix2[2][2]);
